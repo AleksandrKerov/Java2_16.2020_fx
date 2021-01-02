4822d60b368fa0e25157cb6962897d061eb04793
AleksandrKerov <kerovav@mail.ru>
2021-01-02 16:56:10 +0300

    Homework 4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
+
+    @FXML
+    public void clickClose(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    @FXML
+    public void clickBtnSend(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+
+    public void enterText(ActionEvent event) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
